2450f1273741741b0e930684024dcfb2d7ac2589
Asim <149007091+Phantom804@users.noreply.github.com>
2025-09-26 18:37:16 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,10 +33,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="min-h-screen w-full" style={{
-          background: 'radial-gradient(circle at top left, #291638, #130a1d 40%), radial-gradient(circle at bottom right, #4b1a5e, #1d0f2c 40%), linear-gradient(to bottom right, #190d24, #2d1241)',
-          backgroundBlendMode: 'normal'
-        }}>
+        <div
+          className="min-h-screen w-full"
+          style={{
+            background:
+              "radial-gradient(circle at top left, #291638, #130a1d 40%), radial-gradient(circle at bottom right, #4b1a5e, #1d0f2c 40%), linear-gradient(to bottom right, #190d24, #2d1241)",
+            backgroundBlendMode: "normal",
+          }}
+        >
           <div className="w-full">
             <AuthProvider>
               <Toaster position="top-right" richColors />
@@ -46,16 +50,22 @@ export default function RootLayout({
             </AuthProvider>
           </div>
         </div>
-        
-		<!-- Snackeet -->
-		<iframe
-			src="https://snkt.io/68d680e66b20a591a1fd5e79?snkt_channel=iframe"
-			allow="camera *; microphone *; autoplay *; encrypted-media *; fullscreen *;"
-			style="display: block; width:359px; aspect-ratio:359/630;
-			border: none; border-radius: 10px; background: transparent; margin: 0 auto"
-		></iframe>
-		<!-- End Snackeet -->
-		
+
+        {/* Snackeet */}
+        <iframe
+          src="https://snkt.io/68d680e66b20a591a1fd5e79?snkt_channel=iframe"
+          allow="camera *; microphone *; autoplay *; encrypted-media *; fullscreen *;"
+          style={{
+            display: "block",
+            width: "359px",
+            aspectRatio: "359 / 630",
+            border: "none",
+            borderRadius: "10px",
+            background: "transparent",
+            margin: "0 auto",
+          }}
+        />
+        {/* End Snackeet */}
       </body>
     </html>
   );
