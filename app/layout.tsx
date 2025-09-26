951a68ce4a86e9128d6ab561cfef1ee26c5176b9
Asim <149007091+Phantom804@users.noreply.github.com>
2025-09-26 05:22:05 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,16 @@ export default function RootLayout({
             </AuthProvider>
           </div>
         </div>
+        
+		<!-- Snackeet -->
+		<iframe
+			src="https://snkt.io/68d680e66b20a591a1fd5e79?snkt_channel=iframe"
+			allow="camera *; microphone *; autoplay *; encrypted-media *; fullscreen *;"
+			style="display: block; width:359px; aspect-ratio:359/630;
+			border: none; border-radius: 10px; background: transparent; margin: 0 auto"
+		></iframe>
+		<!-- End Snackeet -->
+		
       </body>
     </html>
   );
